bfee287959f6b05373a9e7624805a5e028aab707
SuperNovalx <hoanganhgo9@gmail.com>
2017-04-28 14:08:24 +0700

    New Project

---
+++ Faucet Manager Admin/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Faucet_Manager_Admin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
